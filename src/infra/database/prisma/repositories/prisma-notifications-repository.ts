eab71421aa849d32b58767f6aea28281e8cd6e44
gpmarchi <gustavomarchi@gmail.com>
2022-12-15 15:04:50 -0300

    feat: cancel notification

--- src/infra/database/prisma/repositories/prisma-notifications-repository.ts
+++ src/infra/database/prisma/repositories/prisma-notifications-repository.ts
@@ -7,6 +7,9 @@ import { PrismaService } from '../prisma.service';
 @Injectable()
 export class PrismaNotificationsRepository implements NotificationsRepository {
   constructor(private prismaService: PrismaService) {}
+  async findById(notificationId: string): Promise<Notification | null> {
+    throw new Error('Method not implemented.');
+  }
 
   async create(notification: Notification): Promise<void> {
     const raw = PrismaNotificationMapper.toPrima(notification);
@@ -14,5 +17,9 @@ export class PrismaNotificationsRepository implements NotificationsRepository {
     await this.prismaService.notifications.create({
       data: raw,
     });
+  }
+
+  async save(notification: Notification): Promise<void> {
+    throw new Error('Method not implemented.');
   }
 }
